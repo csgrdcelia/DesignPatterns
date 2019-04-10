4ce52f98bc4b6ebbd8e5af0a41bfd720f91ecbbd
Célia Casagrande <casagrande.celia@gmail.com>
2019-04-10 10:53:23 +0200

    removed comments

--- DotNetCore/DesignPatterns.Tests/FolderFileTest.cs
+++ DotNetCore/DesignPatterns.Tests/FolderFileTest.cs
@@ -10,13 +10,6 @@ namespace DesignPatterns
         [TestMethod]
         public void Should_folder_contains_one_element_when_gettings_contains()
         {
-            /*var textFile = new File { Name = "data.txt" };
-            var parentFolder = new Folder { Name = "Parent Folder" };
-            parentFolder.Files = new List<File> { textFile };
-
-            var containsNumber = parentFolder.GetContainsNumber();
-            
-            Assert.AreEqual(1, containsNumber);*/
             var textFile = new File { Name = "data.txt" };
             var parentFolder = new Folder { Name = "Parent Folder" };
             parentFolder.Elements = new List<IElement>() { textFile };
@@ -37,16 +30,6 @@ namespace DesignPatterns
 
         private static Folder CreateFolderTree()
         {
-            /* var textFile = new File { Name = "data.txt" };
-            var firstFolder = new Folder { Name = "Data Folder" };
-            var secondFolder = new Folder { Name = "Folder" };
-
-            var parentFolder = new Folder { Name = "Parent Folder" };
-            parentFolder.Files = new List<File> { textFile };
-            parentFolder.Folders = new List<Folder> { firstFolder };
-
-            return parentFolder;*/
-
             var textFile = new File { Name = "data.txt" };
             var firstFolder = new Folder { Name = "Data Folder" };
             var secondFolder = new Folder { Name = "Folder" };
@@ -54,7 +37,6 @@ namespace DesignPatterns
             parentFolder.Elements = new List<IElement>() { textFile, firstFolder };
 
             return parentFolder;
-
         }
     }
 }
